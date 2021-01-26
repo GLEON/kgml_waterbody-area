7c64bc0bba9a7b847b6c18213c7f225b44b217ea
hlwander <50301057+hlwander@users.noreply.github.com>
2021-01-26 09:16:39 -0500

    Add files via upload
    wavelet analysis code

---
+++ scripts/wavelet_realsatLakes.R
@@ -0,0 +1,87 @@
+# Lake Expedition wavelet analysis 
+# code adapted from CCC, DWH, and PCH
+# 25 Jan 2021 - HLW
+
+#read in libraries
+pacman::p_load(zoo,dplR,dplyr,tidyverse,ggplot2,ggpubr)
+
+#-------------------------------------------------------------------------------#
+#### User setup generated signal
+NativeSamplingPeriod = 1/12 # normalized to year 
+nYears = 30 # duration
+
+#read in realsat data
+area_timeseries = readRDS("./data/area_timeseries.rds")
+
+#select only relevent cols and remove negative rows
+InData <- area_timeseries %>% select(date, id, area_rm_missing) %>%
+            filter(area_rm_missing >=0) %>% group_by(id) %>%
+            mutate(SeqTime = time(date)) #might have to fix this if looking across multiple lakes
+
+#subset area timeseries to one lake
+InData <- InData[InData$id=="718893",] #647128, 572881, 470900, 718893
+#plot(InData$area_rm_missing~InData$date,type="l")    
+
+nInData = dim(InData)[1]
+
+PlotHeatWave = TRUE # whether or not to plot the heat map of the wavelet transform
+# End user setup
+#------------------------------------------------------------------------------#
+# Wavelet transform
+
+#not subsetting data so...
+  myData = InData
+  SamplingPeriod = NativeSamplingPeriod
+
+#run this so the COI countor appears for entire plot
+options("max.contour.segments" = 250000) # can make number larger if needed 
+
+#Wavelet analysis is this line!
+  output<-morlet(myData$area_rm_missing, myData$SeqTime, dj=1/12, siglvl = 0.95) ###p2 is 2^ whatever value that's set. # NULL sets p2 to 15. #Chose 14.5 so that plot has minimal area above COI
+    #NOTE: had to revert to default p2 because would not plot (I think numbers/dataframe too large??)
+  
+# Post processing wavelet transform
+#setting period to days for plot
+output$period <- round((output$period * SamplingPeriod), digits = 4)  #period is in years here (I think?)
+#View(output$period)
+
+#calculating mean global power per scale period 
+ScalePower <- c(1:97) # make sure this matches power length
+for (j in 1:ncol(output$Power)) {
+  ScalePower[j] <- mean(output$Power[,j])
+}
+
+# create a data frame with the scale period and the mean global power
+powerplot <- as.data.frame(cbind(ScalePower, output$period))
+powerplot <- rename(powerplot, c( "mean_power" = "ScalePower"))
+powerplot <- rename(powerplot, c( "period" = "V2"))
+
+# Filter global power up to 30 years
+powerplotyears <- powerplot %>% 
+  filter(period < nYears)
+
+# Filter global power up to 1 year
+powerplotmonths <- powerplot %>% 
+  filter(period < 1)
+
+#------------------------------------------------------------------------------#
+# FIGURES 
+
+# Plot global power up to several years scale
+plot(powerplotyears$period,powerplotyears$mean_power,type='l',xlab="Peroid (years)",ylab="Mean Power^2")
+grid()
+
+# Plot global power up to several months scale
+plot(powerplotmonths$period,powerplotmonths$mean_power,type='l',xlab="Peroid (months)",ylab="Mean Power^2")
+grid()
+
+#creating plot; THIS CAN TAKE A WHILE. Don't try and run other functions until plot is completely rendered in plot window (R will get angry and crash)
+#make new graphical function to fix period vs. scale issue
+
+#jpeg("./figures/lake_718893_wavelet.jpg", width = 6, height = 4, units = "in",res = 300)
+cols1<-c('blue3', 'blue', "dodgerblue3", "cyan", "green", "greenyellow", "yellow","orange","red", "red3")
+if (PlotHeatWave){
+  print('Plotting heat map may take several minutes...')
+  wavelet.plot(output,key.cols = cols1)
+}
+#dev.off()
