0eb161bbfe910ca8dbb60d7102377aeb8fb77ed3
hlwander <50301057+hlwander@users.noreply.github.com>
2021-03-06 18:24:58 -0500

    nYears and p2
    making nYears depend on length of lake timeseries and playing around with p2 in morlet function

--- scripts/wavelet_realsatLakes.R
+++ scripts/wavelet_realsatLakes.R
@@ -6,10 +6,6 @@
 pacman::p_load(zoo,dplR,dplyr,tidyverse,ggplot2,ggpubr,sf)
 
 #-------------------------------------------------------------------------------#
-#### User setup generated signal
-NativeSamplingPeriod = 1/12 # normalized to year 
-nYears = 30 # duration
-
 #read in realsat data
 area_timeseries = readRDS("./data/area_timeseries.rds")
 USlakes <- read_sf("data/ReaLSAT-R-2.0.shp")
@@ -23,13 +19,17 @@ InData <- area_timeseries %>% select(date, id, area_rm_missing, Reservoir) %>%
             mutate(SeqTime = time(date)) #might have to fix this if looking across multiple lakes
 
 #subset area timeseries to one lake
-InData <- InData[InData$id=="705918",] #lakes: 647128, 572881, 470900, 718893, 712381, 729711, 735202, 698472
+InData <- InData[InData$id=="712490",] #lakes: 647128, 572881, 470900, 718893, 712381, 729711, 735202, 698472
 #plot(InData$area_rm_missing~InData$date,type="l")    #Reservoirs: 712490, 712642, 712700, 712459, 705670, 705918
 
 #normalize data
 InData$area_normalized <- (InData$area_rm_missing - mean(InData$area_rm_missing)) / sd(InData$area_rm_missing)
 
 nInData = dim(InData)[1]
+
+#### User setup generated signal
+NativeSamplingPeriod = 1/12 # normalized to year 
+nYears = length(unique(as.numeric(format(InData$date, "%Y")))) # duration
 
 PlotHeatWave = TRUE # whether or not to plot the heat map of the wavelet transform
 # End user setup
@@ -44,12 +44,12 @@ PlotHeatWave = TRUE # whether or not to plot the heat map of the wavelet transfo
 options("max.contour.segments" = 250000) # can make number larger if needed 
 
 #Wavelet analysis is this line!
-  output<-morlet(myData$area_normalized, myData$SeqTime, dj=1/12, siglvl = 0.95) ###p2 is 2^ whatever value that's set. # NULL sets p2 to 15. #Chose 14.5 so that plot has minimal area above COI
+  output<-morlet(myData$area_normalized, myData$SeqTime, dj=1/12, siglvl = 0.95,p2=9) ###p2 is 2^ whatever value that's set. # NULL sets p2 to 15. #Chose 14.5 so that plot has minimal area above COI
     #NOTE: had to revert to default p2 because would not plot (I think numbers/dataframe too large??)
   
 # Post processing wavelet transform
-#setting period to days for plot
-output$period <- round((output$period * SamplingPeriod), digits = 4)  #period is in years here (I think?)
+#setting period to years for plot
+output$period <- round((output$period * SamplingPeriod), digits = 4) 
 #View(output$period)
 
 #calculating mean global power per scale period 
@@ -85,10 +85,11 @@ grid()
 #creating plot; THIS CAN TAKE A WHILE. Don't try and run other functions until plot is completely rendered in plot window (R will get angry and crash)
 #make new graphical function to fix period vs. scale issue
 
-#jpeg("./figures/lake_705918_wavelet.jpg", width = 6, height = 4, units = "in",res = 300)
+#jpeg("./figures/lake_712490_wavelet_p2=9.jpg", width = 6, height = 4, units = "in",res = 300)
 cols1<-c('blue3', 'blue', "dodgerblue3", "cyan", "green", "greenyellow", "yellow","orange","red", "red3")
 if (PlotHeatWave){
   print('Plotting heat map may take several minutes...')
   wavelet.plot(output,key.cols = cols1)
 }
 #dev.off()
+
