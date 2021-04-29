df65d79ffdcc9231d935a4360cdac185f31e8f31
jsta <stachel2@msu.edu>
2021-04-29 15:25:45 -0400

    randomize row order to ensure geo coverage

--- notebooks/SOURCE/Example_deepclustering_dataprep.py
+++ notebooks/SOURCE/Example_deepclustering_dataprep.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+import random
 
 # csv not committed to repo because it is very large
 dt_raw = pd.read_csv("data/area_timeseries.csv")
@@ -14,12 +15,18 @@ ids = np.random.choice(ids, size=n_lakes, replace=False)
 
 # filter by ids
 dt = dt_raw.loc[dt_raw["id"].isin(ids)]
-ids_save_path = "notebooks/INPUT/REALSAT_TIMESERIES/X_ids.csv"
-ids_save = pd.DataFrame(dt["id"].unique(), columns=["id"])
-ids_save.to_csv(ids_save_path)
 
 # pivot wider
 dt = dt.pivot(index=["id"], values="area_rm_missing", columns=["date"])
+
+# randomize lake order to get full geographic coverage in ML code
+dt_rows = random.sample(range(n_lakes), n_lakes)
+dt = dt.iloc[dt_rows]
+
+ids_save_path = "notebooks/INPUT/REALSAT_TIMESERIES/X_ids.csv"
+ids_save = pd.DataFrame(dt.index, columns=["id"])
+ids_save.to_csv(ids_save_path)
+
 dt = dt.reset_index()
 dt = dt.drop("id", axis=1)
 
