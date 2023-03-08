9bb941fc91cde29973b25f52f40264c33c4a86c8
Heather Wander <hwander@vt.edugit config --global user.name Heather>
2023-03-08 08:52:15 -0500

    querying clusters 6+7

--- scripts/ms_figure_KGML_ML.R
+++ scripts/ms_figure_KGML_ML.R
@@ -1,10 +1,13 @@
+#Manuscript figure 2 and some querying to describe cluster 6 and 7
+#Orignially created by Maartje Korver
+#updated for cluster 6+7 querying on 7 Mar 2023 by HLW
+
 library(data.table)
 library(ggplot2)
 library(here)
 
 kgml = fread("KGML_Clusters_final.csv")
 ml = fread("ML_Clusters_final.csv")
-
 
 
 ggplot(data = kgml[group %in% c(1,2,3,4,5,6,7)]) +
@@ -93,3 +96,19 @@ ggplot(data = ml[cluster %in% c(3,6)]) +
     axis.text.y = element_text(size = 10, color = "black"),
     axis.title = element_blank())
 ggsave("ml_nomatch.png", width = 3.5, height = 2)
+
+
+#Querying KGML clusters 6 and 7:
+clust6 <- kgml[kgml$group==6]
+clust7 <- kgml[kgml$group==7]
+
+#cluster 6 z-score baseline falls between what 2 values? - looks like 0 and 1 using representative waterbody
+ggplot(data = kgml[group %in% c(6,7)]) +
+ # geom_line(aes(x = date, y = observed), color = "black") +
+  geom_line(aes(x = date, y = reconstructed), color = "blue") +
+  facet_grid(rows = vars(group), scales = 'free') +
+  scale_x_date(limits = as.Date(c("1984-01-01","2016-01-01")), breaks = as.Date(c("1984-01-01", "2016-01-01")), date_labels = '%Y') +
+  theme_bw() 
+
+#cluster 7 z-score is more than x standard deviations away from the mean
+sd(clust7$reconstructed) *3
